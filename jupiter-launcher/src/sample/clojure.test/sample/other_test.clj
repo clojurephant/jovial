bb271c2ba76171efcae176e5fad99ac3a2c70143
Andrew Oberstar <ajoberstar@gmail.com>
2016-06-12 14:22:34 -0500

    Better examples of tests

--- jupiter-launcher/src/sample/clojure.test/sample/other_test.clj
+++ jupiter-launcher/src/sample/clojure.test/sample/other_test.clj
@@ -2,10 +2,13 @@
     (:require [clojure.test :refer :all]))
 
 (deftest my-other-works
-  (is (= 1 (- 2 1))))
+  (is (= 1 (- 2 1)) "2 - 1 is 1"))
 
 (deftest my-other-fails
-  (is (= 5 (+ 2 2))))
+  (is (= 5 (+ 2 2)) "2 + 2 is 5"))
+
+(defn do-stuff [_]
+  (throw (ex-info "Yay it works!" {})))
 
 (deftest my-other-error
-  (throw (ex-info "Yay it works!" {})))
+  (is (= 1 (do-stuff 7)) "Do stuff always returns 1"))
